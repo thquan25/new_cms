354f6bd295f9c4de0bcf9aac708aca7c9b01f851
codeatoz <thquan.2510@gmail.com>
2018-12-26 13:39:01 +0700

    Mini Update

--- src/Controller/Admin/DashboardExampleController.php
+++ src/Controller/Admin/DashboardExampleController.php
@@ -7,17 +7,25 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use App\Entity\Article;
 use App\Entity\ArticleCategory;
+use Symfony\Component\HttpFoundation\Request;
 
 class DashboardExampleController extends AbstractController
 {
     private $title = "Example CMS - Dashboard";
     private $breadcrumb = "Dashboard";
 
+    private $categories = null;
+
     /**
      * Config variables for display data table
      * Change them for your purpose
      */
     private $numberOfItems = 20;
+
+    public function __construct()
+    {
+        $this->categories = $this->getDoctrine()->getRepository(ArticleCategory::class)->findAll();
+    }
 
     /**
      * @Route("/admin", name="admin_dashboard")
@@ -32,16 +40,25 @@ class DashboardExampleController extends AbstractController
             'articles' => $articles,
             'categories' => $categories,
         ));
-    }// End here 25/12/2018 Company
+    }
 
     /**
      * @Route("/admin/dashboard/create", name="admin_create_article")
      */
     public function create()
     {
+        $categories = $this->getDoctrine()->getRepository(ArticleCategory::class)->findAll();
         return $this->render('admin/dashboard_example/create.html.twig', array(
             'title' => $this->title,
             'breadcrumb' => $this->breadcrumb . ' - Create article',
+            'categories' => $categories,
         ));
+    }
+
+    /**
+     * @Route("/admin/dashboard/store", name="admin_store_article")
+     */
+    public function store(Request $request)
+    {
     }
 }
